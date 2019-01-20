752c2894040aa62ce1322b79fba2ca40566c1f91
stefnba <stefanjakob.bauer@gmail.com>
2019-01-20 12:31:22 +0100

    To fix file upload

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from .views import (
+    FileImport,
     TransactionList,
 
     Test
@@ -11,8 +12,13 @@ app_name = "main"
 
 urlpatterns = [
 
+    # Import
+    path('file_import/', FileImport.as_view(), name='file-import'),
+
     # Transactions
     path('transactions/', TransactionList.as_view(), name='transaction-list'),
+
+
     
     # Test
     path('test/', Test.as_view(), name='test'),
--- main/views.py
+++ main/views.py
@@ -1,11 +1,13 @@
+from django.http import HttpResponse, HttpResponseServerError
 from django.shortcuts import render
-
 from rest_framework.generics import (
     ListAPIView
 )
-
+from rest_framework.parsers import FormParser, MultiPartParser
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from uuid import uuid4
+
 
 from .models import (
     FileImport,
@@ -27,12 +29,57 @@ from .utils.dd import Barclaycard
     Import
 """""""""""""""""""""""""""""""""""""""""""""
 
-class TransactionList(ListAPIView):
-    """ listing all transactions """
+class ProcessImport(object):
+    def __init__(self, request):
+        self.request = request
+        self.hash_name = None
     
-    queryset = Transaction.objects.all()
-    serializer_class = TransactionListSerializer
+    # for future checks
+    def get_filetype(self):
+        return self.request.FILES['docfile'].content_type
 
+    def get_user(self):
+        return self.request.user.id
+
+    def set_hash_name(self):
+        hash_name = uuid4().hex[:20].upper()
+        self.hash_name = hash_name
+        return hash_name
+
+    """ Methods called by view """
+    def is_valid(self):
+        serializer = FileImportSerializer(data=self.request.data)
+        if serializer.is_valid():
+            # override serializer and save instance to database
+            serializer.save(
+                        user_id=self.get_user(), 
+                        hash_name=self.set_hash_name()
+                    )
+            return True
+        return False
+
+    def get_hash_name(self):
+        return self.hash_name
+
+
+
+class FileImport(APIView):
+    """ Upload .csv file and save to respective folder in Media dir """
+    
+    parser_classes = (MultiPartParser, FormParser,)
+    # serializer_class = ImportSerializer
+
+    def put(self, request, format=None):
+
+        import_transactions = ProcessImport(request)
+        if import_transactions.is_valid():
+            import_key = import_transactions.get_hash_name()
+            return HttpResponse(import_key)
+        else:
+            return HttpResponseServerError()
+
+    def get(self, equest, format=None):
+        return Response('d')
 
 """
     Transactions
