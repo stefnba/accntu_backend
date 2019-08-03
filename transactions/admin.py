46191a055ad79655ecc027c11557478500c868f7
stefnba <stefanjakob.bauer@gmail.com>
2019-08-03 12:28:39 +0200

    admin update

--- transactions/admin.py
+++ transactions/admin.py
@@ -3,6 +3,21 @@ from django.contrib import admin
 # Register your models here.
 from .models import (FX, Transaction)
 
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = (
+        'title', 'date',
+        'spending_amount',
+        'spending_currency',
+        'account_amount',
+        'account_currency',
+        'account',
+    )
+
+    list_filter = (
+        'account',
+        'spending_currency',
+    )
 
 admin.site.register(FX)
-admin.site.register(Transaction)
+admin.site.register(Transaction, TransactionAdmin)
+
